6fe363f555ee3d605cca4aa47baae9b83bb7ed41
Ash-School-42 <jamash0376@students.ecpi.edu>
2026-04-30 09:10:53 -0400

    Create Project Week 4 PhoneNumber.java

---
+++ Project Week 4 PhoneNumber.java
@@ -0,0 +1,18 @@
+/**
+ * Name: James Ash
+ * Date: 04-30-2026
+ * Assignment: 4.2 Week 4 Project
+ * 
+ * Stores phone number (used in composition)
+ **/
+public class PhoneNumber {
+
+    // Stores phone number
+    String number;
+
+    public PhoneNumber(String number) {
+
+        // Set number
+        this.number = number;
+    }
+}
